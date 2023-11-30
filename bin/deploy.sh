231f9410f55db47e088338a5e983aa0a6dd258e8
Daniel Durán Fernández <daniel.duran.sw@gmail.com>
2023-11-30 17:58:56 +0100

    commit base

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,5 +10,13 @@ else
     echo 'Need to follow format ./deploy prod|dev down|up'
 fi
 
-# chmod u+x prod.sh
-# need to use sudo
+# LINUX
+#           sudo chmod u+x prod.sh
+
+# WINDOWS   (To execute the .sh file, we will need a Bash interpreter. Windows Terminal won't work)
+#           icacls ".\bin\deploy.sh" /grant:r "UserName:(RX)"
+
+
+# Execution instructions
+# docker-compose -f docker-compose.yaml -f docker-compose.dev.yaml up --> ./deploy.sh
+# docker-compose -f docker-compose.yaml -f docker-compose.prod.yaml down --> ./deploy.sh 
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,9 +5,9 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello piza world how are you eeekhh!!! fff!</h1>
+                <h1>Hello piza world how are you eeekhh!!!</h1>
                 <h1>Hello world how are you  fff!</h1>
-                <h1>YEAH!!!</h1>
+                <h1>YEAH!!</h1>
                 <h1>{process.env.NODE_ENV}</h1>
             </div>
         );
